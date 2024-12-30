2d829937ed1380c9f759dc8b1d0742871e6acfd4
oussama-zemzemi <oussama.zem99@gmail.com>
2024-12-30 14:14:35 +0100

    adding stuf

---
+++ frontend/src/assets/data/services.js
@@ -0,0 +1,38 @@
+export const services = [
+    {
+      name: "Cancer Care",
+      desc: "We are committed to providing comprehensive and compassionate cancer care. Our dedicated team of oncologists, nurses, and support staff works together to offer advanced treatment options tailored to each patient’s unique needs.",
+      bgColor: "rgba(254, 182, 13, .2)",
+      textColor: "#FEB60D",
+    },
+    {
+      name: "Labor & Delivery",
+      desc: "Welcoming a new life is a moment of joy and celebration, and at HIS, we are here to provide exceptional care throughout your journey to parenthood. Our Labor & Delivery unit offers state-of-the-art facilities, compassionate care, and a safe, comfortable environment for you and your baby.",
+      bgColor: "rgba(151, 113, 255, .2)",
+      textColor: "#9771FF",
+    },
+    {
+      name: "Heart & Vascular",
+      desc: "We are dedicated to providing exceptional care for your heart and vascular health. Our team of highly skilled cardiologists, surgeons, and vascular specialists use advanced technology and innovative techniques to diagnose, treat, and prevent cardiovascular diseases.",
+      bgColor: "rgba(1, 181, 197, .2)",
+      textColor: "#01B5C5",
+    },
+    {
+      name: "Mental Health",
+      desc: "We understand the importance of mental well-being in leading a fulfilling and healthy life. Our expert mental health professionals are dedicated to providing compassionate care and support for individuals facing mental health challenges.",
+      bgColor: "rgba(1, 181, 197, .2)",
+      textColor: "#01B5C5",
+    },
+    {
+      name: "Neurology",
+      desc: "We are committed to providing comprehensive care for patients with neurological disorders. Our expert team of neurologists, neurosurgeons, and specialized staff utilize cutting-edge technology and the latest advancements in medical science to diagnose, treat, and manage conditions affecting the brain, spine, and nervous system.",
+      bgColor: "rgba(254, 182, 13, .2)",
+      textColor: "#FEB60D",
+    },
+    {
+      name: "Burn Treatment",
+      desc: "We provide specialized care for burn injuries, offering advanced treatment options and compassionate support to help patients recover physically and emotionally. Our Burn Care Unit is equipped with the latest technology and staffed by a skilled, multidisciplinary team dedicated to delivering comprehensive care for all types and severities of burns.",
+      bgColor: "rgba(151, 113, 255, .2)",
+      textColor: "#9771FF",
+    },
+  ];
